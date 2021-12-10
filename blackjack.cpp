1bff97c89f49aafa1a4235a7a0d1e3d57764cbfb
waterbottle12 <95153447+waterbottle12@users.noreply.github.com>
2021-12-10 11:38:36 -0600

    Update blackjack.cpp

--- blackjack.cpp
+++ blackjack.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <bits/stdc++.h>
 #include <unistd.h>
+#include <climits>
 
 std::vector<int> deck = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
 std::vector<int> globalCards;
@@ -9,12 +10,67 @@ std::vector<int> globalCards;
 class Player{
 private:
     int total = 0;
+    int money;
+    int bet;
     std::vector<int> cards;
 public:
     void clearCards()
     {
         cards.clear();
         total = 0;
+    }
+    void moneyReset()
+    {
+        money = 1000;
+    }
+    int getMoney()
+    {
+        return money;
+    }
+    void playerBet()
+    {
+        do
+        {
+            std::cout << "Place wager(increments of 5): " << std::endl;
+            std::cin >> bet;
+            while(std::cin.fail())
+            {
+                std::cin.clear();
+                std::cin.ignore(INT_MAX, '\n');    
+                std::cout << "Invalid entry, try again" << std::endl;
+                std::cin >> bet;
+            }
+            if(bet % 5 == 0)
+            {
+                std::cout << "You wager: " << bet << std::endl;
+                money -= bet;
+                break;
+            }
+            else if(bet % 5 != 0)
+                std::cout << "Invalid entry, try again" << std::endl;
+        }while(1);       
+    }
+    int getBet()
+    {
+        return bet;
+    }
+    int winPayout()
+    {
+        money += (bet * 2);
+        return bet;
+    }
+    int BJwinPayout()
+    {
+        money += (bet * 2.5);
+        return bet * 1.5;
+    }
+    void losePayout()
+    {
+        bet = 0;
+    }
+    void tiePayout()
+    {
+        money += bet
     }
     int hit()
     {
@@ -54,6 +110,40 @@ public:
         std::cout << "1 - Hit" << std::endl;
         std::cout << "2 - Stand" << std::endl;
     }
+    bool playerBjack()
+    {
+        if(cards[0] == 1 && cards[1] == 10 || cards[0] == 10 && cards[1] == 1)
+            return true;
+        return false;
+    }
+    bool dealerBjack()
+    {
+        if(cards[0] == 10 && cards[1] == 1)
+            return true;
+        return false;
+    }
+    int hard1()
+    {
+        if(cards[0] == 1)
+        {
+            int choice;
+            std::cout << "Would you like to have your ace worth 1 or 11" << std::endl;
+            std::cin >> choice;
+            if(choice == 11)
+                cards[0] += 10;
+            return 0;
+        }
+        if(cards[1] == 1)
+        {
+            int choice;
+            std::cout << "Would you like to have your ace worth 1 or 11" << std::endl;
+            std::cin >> choice;
+            if(choice == 11)
+                cards[1] += 10;
+            return 0;
+        }
+        return 0;
+    }
 };
 void gameStart(Player &player, Player &dealer)
 {
@@ -90,22 +180,64 @@ int main()
 {   
     Player player;
     Player dealer;
+    player.moneyReset();
     int choice;
     char startComm;
     bool gameLose = false;
     bool game = false;
+    bool blackjack = false;
     
     std::cout << "+ to deal" << std::endl;
     std::cin >> startComm;
-    if(startComm == '+')
-        bool game = true;
-    else
-        std::cout << "Invalid Entry";
+    do
+    {
+        if(startComm == '+')
+        {
+            bool game = true;
+            std::cout << "Balance: " << player.getMoney() << std::endl;
+            player.playerBet();
+            break;
+        }
+        else
+            std::cout << "Invalid Entry, try again" << std::endl;
+            std::cin >> startComm;
+    }while(game == false);
     do
     {
         gameStart(player, dealer);
-        player.hitStand();
-        std::cin >> choice;
+        if(player.playerBjack() == true && dealer.dealerBjack() == false)
+        {
+            std::cout << "Blackjack!" << std::endl;
+            std::cout << "+ " << player.BJwinPayout() << std::endl;
+            blackjack = true;
+        }
+        else if(player.playerBjack() == true && dealer.dealerBjack() == true)
+        {
+            std::cout << "Blackjack!" << std::endl;
+            std::cout << "Tie" << std::endl;
+            player.tiePayout();
+            blackjack = true;
+        }
+        else if(player.playerBjack() == false && dealer.dealerBjack() == true)
+        {
+            std::cout << "Dealer blackjack!" << std::endl;
+            std::cout << "Player lose" << std::endl;
+            player.losePayout();
+            blackjack = true;
+        }
+        else
+        {
+            player.hard1();
+            player.hitStand();
+            std::cin >> choice;
+            while(std::cin.fail())
+            {
+                std::cin.clear();
+                std::cin.ignore(INT_MAX, '\n');    
+                std::cout << "Invalid entry, try again" << std::endl;
+                std::cin >> choice;
+            }
+        }
         while(choice == 1)
         {
                 player.getCards();
@@ -116,14 +248,15 @@ int main()
                 if(player.getTotal() > 21)
                 {
                     usleep(300000);
-                    std::cout << "BUST!" << std::endl;
+                    std::cout << "Bust!" << std::endl;
+                    player.losePayout();
                     gameLose = true;
                     break;
                 }
                 player.hitStand();
                 std::cin >> choice;
         }
-        if(gameLose == false)
+        if(gameLose == false && blackjack == false)
         {
             std::cout << "Dealer reveals: " << std::endl;
             usleep(300000);
@@ -142,8 +275,8 @@ int main()
             if(dealer.getTotal() > 21)
             {
                 usleep(300000);
-                std::cout << "DEALER BUST!" << std::endl;
-                std::cout << "Payout" << std::endl;
+                std::cout << "Dealer bust!" << std::endl;
+                std::cout << "+ " << player.winPayout() << std::endl;
             }
             else
             {
@@ -155,13 +288,20 @@ int main()
                 {
                     usleep(500000);
                     std::cout << "Player win with: " << player.getTotal() << std::endl;
-                    std::cout << "Payout" << std::endl;
+                    std::cout << "+ " << player.winPayout() << std::endl;
                 }
                 else if(player.getTotal() < dealer.getTotal())
                 {
                     usleep(500000);
                     std::cout << "Player win loses with: " << player.getTotal() << std::endl;
+                    player.losePayout();
                     gameLose = true;
+                }
+                else if(player.getTotal() == dealer.getTotal())
+                {
+                    usleep(500000);
+                    std::cout << "Tie" << std::endl;
+                    player.tiePayout();
                 }
             }
         }
@@ -169,21 +309,37 @@ int main()
         std::cout << "+ to play again" << std::endl;
         std::cout << "Any other key to exit" << std::endl;
         std::cin >> startComm;
+        while(std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(INT_MAX, '\n');    
+            std::cout << "Invalid entry, try again" << std::endl;
+            std::cin >> startComm;
+        }
         if(startComm == '+')
         {
-            game = false;
-            player.clearCards();
-            dealer.clearCards();
-            gameLose = false;
+            if(player.getMoney() != 0)
+            {
+                game = true;
+                player.clearCards();
+                dealer.clearCards();
+                gameLose = false;
+                blackjack = false;
+                std::cout << "Balance: " << player.getMoney() << std::endl;
+                player.playerBet();
+            }
+            else if(player.getMoney() == 0)
+            {
+                std::cout << "Balance: " << player.getMoney() << std::endl;
+                std::cout << "You're out of money, you lose!" << std::endl;
+                startComm == '-';
+                break;
+            }
         }
         else
             break;
-    } while(game == false);
-    std::cout << "fuck you" << std::endl;
+    } while(game == true);
+    std::cout << "You finish with: " << player.getMoney() << std::endl;
 }
 
-//ties/getting blackjack
-//betting
-//dealer revealing he has blackjack
 //splits
-//cin flushing
